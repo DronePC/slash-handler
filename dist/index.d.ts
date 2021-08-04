5f092cb39b19aa23ff3b972d65b29f3339778101
DronePC <54619416+DronePC@users.noreply.github.com>
2021-08-04 20:34:11 +0300

    Delete dist directory
    Forgot to gitignore the build directory.

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Command, CommandGroup } from "./src/Command";
-export { default as CommandHandler } from "./src/CommandHandler";
-export { ButtonRow, SelectMenuRow } from './src/ActionRow';
-export { FunctionButton, DisabledButton, LinkButton } from "./src/Buttons";
